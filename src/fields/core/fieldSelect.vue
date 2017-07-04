386524fedf788e80f308ffddaf725b14368e5214
mepsmike <mepsmike@gmail.com>
2017-07-04 23:18:27 +0800

    fix: change select option

--- src/fields/core/fieldSelect.vue
+++ src/fields/core/fieldSelect.vue
@@ -1,13 +1,12 @@
 <template lang="pug">
 	select.form-control(v-model="value", :disabled="disabled", :name="schema.inputName", :id="getFieldID(schema)")
-		option(:disabled="schema.required", :value="null", :selected="value == undefined") {{ selectOptions.noneSelectedText || "&lt;Nothing selected&gt;" }}
 		option(v-for="item in items", :value="getItemID(item)") {{ getItemName(item) }}
 </template>
 
 <script>
 	import {isObject} from "lodash";
 	import abstractField from "../abstractField";
-	
+
 	export default {
 		mixins: [ abstractField ],
 
@@ -22,7 +21,7 @@
 					return values.apply(this, [this.model, this.schema]);
 				} else
 					return values;
-			}      
+			}
 		},
 
 		methods: {
